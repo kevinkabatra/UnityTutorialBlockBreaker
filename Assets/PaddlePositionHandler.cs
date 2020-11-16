39299ed7175ff19ddf7deeaf65d06febc7fa2225
Kevin Kabatra <kevin@lsretail.com>
2020-11-16 03:13:45 +0000

    Able to move the paddle around now.

---
+++ Assets/PaddlePositionHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PaddlePositionHandler : MonoBehaviour
+{
+    [SerializeField] const float screenWidthInUnits = 16f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        var newPaddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+        transform.position = newPaddlePosition;
+    }
+}
