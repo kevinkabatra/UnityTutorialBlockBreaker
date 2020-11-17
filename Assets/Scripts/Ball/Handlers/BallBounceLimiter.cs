d5fdeee20264c47a44a1958a7a540cf5f19ba91b
Kevin Kabatra <kevin@lsretail.com>
2020-11-17 02:18:43 +0000

    Ball now hugs the paddle, also organized some of the scripts

---
+++ Assets/Scripts/Ball/Handlers/BallBounceLimiter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class BallBounceLimiter : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // ToDo: figure out how to slow down the ball once it is going to fast.       
+    }
+}
---
+++ Assets/Scripts/Ball/Handlers/BallPositionHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BallPositionHandler : MonoBehaviour
+{
+    [SerializeField] PaddleGameObject paddle;
+    Vector2 paddleToBallVector;
+
+    void Start()
+    {
+        paddleToBallVector = GetPaddleToBallVector();
+    }
+
+    void Update()
+    {
+        KeepBallAttachedToPaddle();
+    }
+
+    private Vector2 GetPaddleToBallVector()
+    {
+        return transform.position - paddle.transform.position;
+    }
+
+    private void KeepBallAttachedToPaddle()
+    {
+        var paddleCurrentPosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddleCurrentPosition + paddleToBallVector;
+    }
+}
