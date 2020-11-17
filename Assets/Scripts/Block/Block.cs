3ee81de084a22d9af755e9f90a64910a595e78c0
Kevin Kabatra <kevin@lsretail.com>
2020-11-17 12:34:55 +0000

    The block can now take some damage

---
+++ Assets/Scripts/Block/Block.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private int damageLevel = 0;
+    SpriteRenderer spriteRenderer;
+
+    [SerializeField] private Sprite blockOneDamage;
+    [SerializeField] private Sprite blockTwoDamage;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        HandleDamage();
+    }
+
+    private void HandleDamage()
+    {
+        damageLevel++;
+
+        switch(damageLevel)
+        {
+            case 1:
+                spriteRenderer.sprite = blockOneDamage; 
+                break;
+            case 2:
+                spriteRenderer.sprite = blockTwoDamage;
+                break;
+            case 3:
+                Destroy(gameObject);
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+    }
+}
