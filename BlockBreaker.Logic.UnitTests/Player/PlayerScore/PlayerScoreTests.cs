d357994a02d9b364e18778f714c872e9361da043
Kevin Kabatra <kevin@lsretail.com>
2020-11-21 11:49:58 +0000

    Added some unit testing for the logic layer

---
+++ BlockBreaker.Logic.UnitTests/Player/PlayerScore/PlayerScoreTests.cs
@@ -0,0 +1,33 @@
+﻿namespace BlockBreaker.Logic.UnitTests.Player.PlayerScore
+{
+    using BlockBreaker.Logic.Player.PlayerScore;
+    using Xunit;
+
+    public class PlayerScoreTests
+    {
+        [Fact]
+        public void CanAddToScore()
+        {
+            var playerScore = PlayerScore.GetOrCreateInstance();
+            var initialScore = playerScore.GetScore();
+
+            playerScore.AddToScore();
+            var updatedScore = playerScore.GetScore();
+
+            Assert.Equal(updatedScore, initialScore + 1);
+        }
+
+        [Fact]
+        public void CanAddSpecifiedPointsToScore()
+        {
+            var playerScore = PlayerScore.GetOrCreateInstance();
+            var initialScore = playerScore.GetScore();
+
+            var pointsToAdd = 2;
+            playerScore.AddToScore(pointsToAdd);
+            var updatedScore = playerScore.GetScore();
+
+            Assert.Equal(updatedScore, initialScore + pointsToAdd);
+        }
+    }
+}
